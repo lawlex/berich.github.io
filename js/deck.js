a9d06dafd83e21c674c806666cdc4dcf23caa992
Alexey <alex.kuznecov@gmail.com>
2020-03-01 21:25:38 +0300

    Update deck.js

--- js/deck.js
+++ js/deck.js
@@ -9,7 +9,7 @@ var translate = Deck.translate
 
 var $container = document.getElementById('container')
 var $topbar = document.getElementById('topbar')
-var $navbar = document.getElementById('navbar')
+var $navbar = document.createElement('ul').class('navbar-nav')
 
 var $sort = document.createElement('button')
 var $shuffle = document.createElement('button')
