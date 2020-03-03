49a81fea689c05686fad8aec43b39dfb19728840
Alexey <alex.kuznecov@gmail.com>
2020-03-03 21:48:37 +0300

    Update

--- js/deck.js
+++ js/deck.js
@@ -1,304 +1,304 @@
 
-  /* global Deck */
+/* global Deck */
 
-  var prefix = Deck.prefix;
+var prefix = Deck.prefix;
 
-  var transform = prefix('transform');
+var transform = prefix('transform');
 
-  var translate = Deck.translate;
+var translate = Deck.translate;
 
-  var $container = document.getElementById('container');
-  var $topbar = document.getElementById('topbar');
+var $container = document.getElementById('container');
+var $topbar = document.getElementById('topbar');
 
-  var $sort = document.createElement('button');
-  var $shuffle = document.createElement('button');
-  var $bysuit = document.createElement('button');
-  var $fan = document.createElement('button');
-  var $poker = document.createElement('button');
-  var $flip = document.createElement('button');
+var $sort = document.createElement('button');
+var $shuffle = document.createElement('button');
+var $bysuit = document.createElement('button');
+var $fan = document.createElement('button');
+var $poker = document.createElement('button');
+var $flip = document.createElement('button');
 
-  $shuffle.textContent = 'Shuffle';
-  $sort.textContent = 'Sort';
-  $bysuit.textContent = 'By suit';
-  $fan.textContent = 'Fan';
-  $poker.textContent = 'Poker';
-  $flip.textContent = 'Flip';
-  /*
-  $topbar.appendChild($flip);
-  $topbar.appendChild($shuffle);
-  $topbar.appendChild($bysuit);
-  $topbar.appendChild($fan);
-  $topbar.appendChild($poker);
-  $topbar.appendChild($sort);
-  */
-  // navbar
-  var $_topbar = $('#topbar');
-  var $_navbar = $('<ul class="navbar-nav"></ul>');
+$shuffle.textContent = 'Shuffle';
+$sort.textContent = 'Sort';
+$bysuit.textContent = 'By suit';
+$fan.textContent = 'Fan';
+$poker.textContent = 'Poker';
+$flip.textContent = 'Flip';
+/*
+$topbar.appendChild($flip);
+$topbar.appendChild($shuffle);
+$topbar.appendChild($bysuit);
+$topbar.appendChild($fan);
+$topbar.appendChild($poker);
+$topbar.appendChild($sort);
+*/
+// navbar
+var $_topbar = $('#topbar');
+var $_navbar = $('<ul class="navbar-nav"></ul>');
 
-  var $_lisort = $('<li class="nav-item"><button id="btn_sort" class="nav-link">Sort</button></li>');
-  var $_lishuffle = $('<li class="nav-item"><button id="btn_shuffle" class="nav-link">Shuffle</button></li>');
-  var $_libysuit = $('<li class="nav-item"><button id="btn_bysuit" class="nav-link">By suit</button></li>');
-  var $_lifan = $('<li class="nav-item"><button id="btn_fan" class="nav-link">Fan</button></li>');
-  var $_lipoker = $('<li class="nav-item"><button id="btn_poker" class="nav-link">Poker</button></li>');
-  var $_liflip = $('<li class="nav-item"><button id="btn_flip" class="nav-link">Flip</button></li>');
+var $_lisort = $('<li class="nav-item"><button id="btn_sort" class="nav-link">Sort</button></li>');
+var $_lishuffle = $('<li class="nav-item"><button id="btn_shuffle" class="nav-link">Shuffle</button></li>');
+var $_libysuit = $('<li class="nav-item"><button id="btn_bysuit" class="nav-link">By suit</button></li>');
+var $_lifan = $('<li class="nav-item"><button id="btn_fan" class="nav-link">Fan</button></li>');
+var $_lipoker = $('<li class="nav-item"><button id="btn_poker" class="nav-link">Poker</button></li>');
+var $_liflip = $('<li class="nav-item"><button id="btn_flip" class="nav-link">Flip</button></li>');
 
-  $_navbar.append($_liflip, $_lishuffle, $_libysuit, $_lifan, $_lipoker, $_lisort);
+$_navbar.append($_liflip, $_lishuffle, $_libysuit, $_lifan, $_lipoker, $_lisort);
 
-  //var $_sort = $('btn_sort');
-  //var $_shuffle = $('btn_shuffle');
-  var $_bysuit = $('btn_bysuit');
-  var $_fan = $('btn_fan');
-  var $_poker = $('btn_poker');
-  var $_flip = $('btn_flip');
-  /*
-  <li class="nav-item">
+//var $_sort = $('btn_sort');
+//var $_shuffle = $('btn_shuffle');
+var $_bysuit = $('btn_bysuit');
+var $_fan = $('btn_fan');
+var $_poker = $('btn_poker');
+var $_flip = $('btn_flip');
+/*
+<li class="nav-item">
 
-        <a class="nav-link" href="#">Link</a>
+      <a class="nav-link" href="#">Link</a>
 
-      </li>
-  */
+    </li>
+*/
 
-  $_topbar.append($_navbar);
-  // end navbar
+$_topbar.append($_navbar);
+// end navbar
 
-  var deck = Deck();
+var deck = Deck();
 
-  // easter eggs start
+// easter eggs start
 
-  var acesClicked = [];
-  var kingsClicked = [];
+var acesClicked = [];
+var kingsClicked = [];
 
-  deck.cards.forEach(function (card, i) {
-    card.enableDragging();
-    card.enableFlipping();
+deck.cards.forEach(function (card, i) {
+  card.enableDragging();
+  card.enableFlipping();
 
-    card.$el.addEventListener('mousedown', onTouch);
-    card.$el.addEventListener('touchstart', onTouch);
+  card.$el.addEventListener('mousedown', onTouch);
+  card.$el.addEventListener('touchstart', onTouch);
 
-    function onTouch () {
-      var card;
+  function onTouch () {
+    var card;
 
-      if (i % 13 === 0) {
-        acesClicked[i] = true;
-        if (acesClicked.filter(function (ace) {
-          return ace;
-        }).length === 4) {
-          document.body.removeChild($topbar);
-          deck.$el.style.display = 'none';
-          setTimeout(function () {
-            startWinning();
-          }, 250)
+    if (i % 13 === 0) {
+      acesClicked[i] = true;
+      if (acesClicked.filter(function (ace) {
+        return ace;
+      }).length === 4) {
+        document.body.removeChild($topbar);
+        deck.$el.style.display = 'none';
+        setTimeout(function () {
+          startWinning();
+        }, 250)
+      }
+    } else if (i % 13 === 12) {
+      if (!kingsClicked) {
+        return;
+      }
+      kingsClicked[i] = true;
+      if (kingsClicked.filter(function (king) {
+        return king;
+      }).length === 4) {
+        for (var j = 0; j < 3; j++) {
+          card = Deck.Card(52 + j);
+          card.mount(deck.$el);
+          card.$el.style[transform] = 'scale(0)';
+          card.setSide('front');
+          card.enableDragging();
+          card.enableFlipping();
+          deck.cards.push(card);
         }
-      } else if (i % 13 === 12) {
-        if (!kingsClicked) {
-          return;
-        }
-        kingsClicked[i] = true;
-        if (kingsClicked.filter(function (king) {
-          return king;
-        }).length === 4) {
-          for (var j = 0; j < 3; j++) {
-            card = Deck.Card(52 + j);
-            card.mount(deck.$el);
-            card.$el.style[transform] = 'scale(0)';
-            card.setSide('front');
-            card.enableDragging();
-            card.enableFlipping();
-            deck.cards.push(card);
-          }
-          deck.sort(true);
-          kingsClicked = false;
-        }
-      } else {
-        acesClicked = [];
-        if (kingsClicked) {
-          kingsClicked = [];
-        }
+        deck.sort(true);
+        kingsClicked = false;
+      }
+    } else {
+      acesClicked = [];
+      if (kingsClicked) {
+        kingsClicked = [];
       }
     }
-  })
+  }
+})
 
-  function startWinning () {
-    var $winningDeck = document.createElement('div');
-    $winningDeck.classList.add('deck');
+function startWinning () {
+  var $winningDeck = document.createElement('div');
+  $winningDeck.classList.add('deck');
 
-    $winningDeck.style[transform] = translate(Math.random() * window.innerWidth - window.innerWidth / 2 + 'px', Math.random() * window.innerHeight - window.innerHeight / 2 + 'px');
+  $winningDeck.style[transform] = translate(Math.random() * window.innerWidth - window.innerWidth / 2 + 'px', Math.random() * window.innerHeight - window.innerHeight / 2 + 'px');
 
-    $container.appendChild($winningDeck);
+  $container.appendChild($winningDeck);
 
-    var side = Math.floor(Math.random() * 2) ? 'front' : 'back';
+  var side = Math.floor(Math.random() * 2) ? 'front' : 'back';
 
-    for (var i = 0; i < 55; i++) {
-      addWinningCard($winningDeck, i, side);
-    }
-
-    setTimeout(startWinning, Math.round(Math.random() * 1000));
+  for (var i = 0; i < 55; i++) {
+    addWinningCard($winningDeck, i, side);
   }
 
-  function addWinningCard ($deck, i, side) {
-    var card = Deck.Card(54 - i);
-    var delay = (55 - i) * 20;
-    var animationFrames = Deck.animationFrames;
-    var ease = Deck.ease;
+  setTimeout(startWinning, Math.round(Math.random() * 1000));
+}
 
-    card.enableFlipping();
+function addWinningCard ($deck, i, side) {
+  var card = Deck.Card(54 - i);
+  var delay = (55 - i) * 20;
+  var animationFrames = Deck.animationFrames;
+  var ease = Deck.ease;
 
-    if (side === 'front') {
-      card.setSide('front');
-    } else {
-      card.setSide('back');
-    }
+  card.enableFlipping();
 
-    card.mount($deck);
-    card.$el.style.display = 'none';
-
-    var xStart = 0;
-    var yStart = 0;
-    var xDiff = -500;
-    var yDiff = 500;
-
-    animationFrames(delay, 1000)
-      .start(function () {
-        card.x = 0;
-        card.y = 0;
-        card.$el.style.display = '';
-      })
-      .progress(function (t) {
-        var tx = t;
-        var ty = ease.cubicIn(t);
-        card.x = xStart + xDiff * tx;
-        card.y = yStart + yDiff * ty;
-        card.$el.style[transform] = translate(card.x + 'px', card.y + 'px');
-      })
-      .end(function () {
-        card.unmount();
-      })
+  if (side === 'front') {
+    card.setSide('front');
+  } else {
+    card.setSide('back');
   }
 
-  // easter eggs end
-  // new buttons
+  card.mount($deck);
+  card.$el.style.display = 'none';
 
-  $('#btn_shuffle').click(function(){
-    // shuffle
-    deck.shuffle();
-    deck.shuffle();
-  });
-  $('#btn_sort').click(function(){
-    // sort
-    deck.sort();
-  });
-  $('#btn_bysuit').click(function(){
-    // bysuit
-    deck.sort(true); // sort reversed
-    deck.bysuit();
-  });
-  $('#btn_fan').click(function(){
-    // fan
-    deck.fan();
-  });
-  $('#btn_flip').click(function(){
-    // flip
-    deck.flip();
-  });
-  $('#btn_poker').click(function(){
-    // poker
+  var xStart = 0;
+  var yStart = 0;
+  var xDiff = -500;
+  var yDiff = 500;
 
-    deck.queue(function (next) {
-      deck.cards.forEach(function (card, i) {
-        setTimeout(function () {
-          card.setSide('back');
-        }, i * 7.5);
-      })
-      next();
+  animationFrames(delay, 1000)
+    .start(function () {
+      card.x = 0;
+      card.y = 0;
+      card.$el.style.display = '';
     })
-    deck.shuffle();
-    deck.shuffle();
-    deck.poker();
-  });
-  // end new buttons
-  // old buttons
-  /*
-  $shuffle.addEventListener('click', function () {
-    deck.shuffle();
-    deck.shuffle();
-  })
-  $sort.addEventListener('click', function () {
-    deck.sort();
-  })
-  $bysuit.addEventListener('click', function () {
-    deck.sort(true); // sort reversed
-    deck.bysuit();
-  })
-  $fan.addEventListener('click', function () {
-    deck.fan();
-  })
-  $flip.addEventListener('click', function () {
-    deck.flip();
-  })
-  $poker.addEventListener('click', function () {
-    deck.queue(function (next) {
-      deck.cards.forEach(function (card, i) {
-        setTimeout(function () {
-          card.setSide('back');
-        }, i * 7.5);
-      })
-      next();
+    .progress(function (t) {
+      var tx = t;
+      var ty = ease.cubicIn(t);
+      card.x = xStart + xDiff * tx;
+      card.y = yStart + yDiff * ty;
+      card.$el.style[transform] = translate(card.x + 'px', card.y + 'px');
     })
-    deck.shuffle();
-    deck.shuffle();
-    deck.poker();
-  })
-  */
-  // end old buttons
-  deck.mount($container);
+    .end(function () {
+      card.unmount();
+    })
+}
 
-  deck.intro();
+// easter eggs end
+// new buttons
+
+$('#btn_shuffle').click(function(){
+  // shuffle
+  deck.shuffle();
+  deck.shuffle();
+});
+$('#btn_sort').click(function(){
+  // sort
   deck.sort();
+});
+$('#btn_bysuit').click(function(){
+  // bysuit
+  deck.sort(true); // sort reversed
+  deck.bysuit();
+});
+$('#btn_fan').click(function(){
+  // fan
+  deck.fan();
+});
+$('#btn_flip').click(function(){
+  // flip
+  deck.flip();
+});
+$('#btn_poker').click(function(){
+  // poker
 
-  // secret message..
+  deck.queue(function (next) {
+    deck.cards.forEach(function (card, i) {
+      setTimeout(function () {
+        card.setSide('back');
+      }, i * 7.5);
+    })
+    next();
+  })
+  deck.shuffle();
+  deck.shuffle();
+  deck.poker();
+});
+// end new buttons
+// old buttons
+/*
+$shuffle.addEventListener('click', function () {
+  deck.shuffle();
+  deck.shuffle();
+})
+$sort.addEventListener('click', function () {
+  deck.sort();
+})
+$bysuit.addEventListener('click', function () {
+  deck.sort(true); // sort reversed
+  deck.bysuit();
+})
+$fan.addEventListener('click', function () {
+  deck.fan();
+})
+$flip.addEventListener('click', function () {
+  deck.flip();
+})
+$poker.addEventListener('click', function () {
+  deck.queue(function (next) {
+    deck.cards.forEach(function (card, i) {
+      setTimeout(function () {
+        card.setSide('back');
+      }, i * 7.5);
+    })
+    next();
+  })
+  deck.shuffle();
+  deck.shuffle();
+  deck.poker();
+})
+*/
+// end old buttons
+deck.mount($container);
 
-  var randomDelay = 10000 + 30000 * Math.random();
+deck.intro();
+deck.sort();
 
-  setTimeout(function () {
-    printMessage('Psst..I want to share a secret with you...');
-  }, randomDelay);
+// secret message..
 
-  setTimeout(function () {
-    printMessage('...try clicking all kings and nothing in between...');
-  }, randomDelay + 5000);
+var randomDelay = 10000 + 30000 * Math.random();
 
-  setTimeout(function () {
-    printMessage('...have fun ;)');
-  }, randomDelay + 10000);
+setTimeout(function () {
+  printMessage('Psst..I want to share a secret with you...');
+}, randomDelay);
 
-  function printMessage (text) {
-    var animationFrames = Deck.animationFrames;
-    var ease = Deck.ease;
-    var $message = document.createElement('p');
-    $message.classList.add('message');
-    $message.textContent = text;
+setTimeout(function () {
+  printMessage('...try clicking all kings and nothing in between...');
+}, randomDelay + 5000);
 
-    document.body.appendChild($message);
+setTimeout(function () {
+  printMessage('...have fun ;)');
+}, randomDelay + 10000);
 
-    $message.style[transform] = translate(window.innerWidth + 'px', 0);
+function printMessage (text) {
+  var animationFrames = Deck.animationFrames;
+  var ease = Deck.ease;
+  var $message = document.createElement('p');
+  $message.classList.add('message');
+  $message.textContent = text;
 
-    var diffX = window.innerWidth;
+  document.body.appendChild($message);
 
-    animationFrames(1000, 700)
-      .progress(function (t) {
-        t = ease.cubicInOut(t);
-        $message.style[transform] = translate((diffX - diffX * t) + 'px', 0);
-      })
+  $message.style[transform] = translate(window.innerWidth + 'px', 0);
 
-    animationFrames(6000, 700)
-      .start(function () {
-        diffX = window.innerWidth;
-      })
-      .progress(function (t) {
-        t = ease.cubicInOut(t);
-        $message.style[transform] = translate((-diffX * t) + 'px', 0);
-      })
-      .end(function () {
-        document.body.removeChild($message);
-      })
-  }
+  var diffX = window.innerWidth;
+
+  animationFrames(1000, 700)
+    .progress(function (t) {
+      t = ease.cubicInOut(t);
+      $message.style[transform] = translate((diffX - diffX * t) + 'px', 0);
+    })
+
+  animationFrames(6000, 700)
+    .start(function () {
+      diffX = window.innerWidth;
+    })
+    .progress(function (t) {
+      t = ease.cubicInOut(t);
+      $message.style[transform] = translate((-diffX * t) + 'px', 0);
+    })
+    .end(function () {
+      document.body.removeChild($message);
+    })
+}
