b45f02ad893ffa025a0747e53cc8b443cdecda5a
lawlex <alex.kuznecov@gmail.com>
2020-05-21 21:13:41 +0300

    Update

--- openbook/js/main.js
+++ openbook/js/main.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
             'class': 'input-group-prepend',
         }),
     }).appendTo('#tablewrapper');
+    // hand
     $('<div>', {
         'class': 'input-group-text',
         append: $('<div/>', {
@@ -40,95 +41,95 @@ $(document).ready(function() {
     var handhtml = $('<div/>', {
         class: 'input-group-text',
     }).appendTo('#tablecontainer');
-    var handcard1 = handhtml.append($(
-        `<div id='handcard1' class='card playing rank-a suit-hearts'>
-          <div class='card-body'>
-              <div class='card-title'></div>
-              <div class='card-text'></div>
-          </div>
-          </div>`, {}));
-    var handcard2 = handhtml.append($(
-        `<div id='handcard1' class='card playing rank-a suit-spades'>
-            <div class='card-body'>
-                <div class='card-title'></div>
-                <div class='card-text'></div>
-            </div>
-            </div>`, {}));
 
-    /*
-    <div class="input-group mb-3">
-            <div class="input-group-prepend">
-                <div class="input-group-text">
-                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                        <label class="btn btn-secondary">
-                        <input type="checkbox" name="options" id="btnusehand" autocomplete="off" checked> Рука: </label>
-                    </div>
-                </div>
+    // add playing card's html colde
+    function addplayingcard(_parent, _id, _nom, _suit, _class = 'card playing') {
+        var _noms = {
+            'E': { 'id': 100, 'cls': 'emptyplace' },
+            '2': { 'id': 1, 'cls': 'rank-2' },
+            '3': { 'id': 2, 'cls': 'rank-3' },
+            '4': { 'id': 3, 'cls': 'rank-4' },
+            '5': { 'id': 4, 'cls': 'rank-5' },
+            '6': { 'id': 5, 'cls': 'rank-6' },
+            '7': { 'id': 6, 'cls': 'rank-7' },
+            '8': { 'id': 7, 'cls': 'rank-8' },
+            '9': { 'id': 8, 'cls': 'rank-9' },
+            'T': { 'id': 9, 'cls': 'rank-t' },
+            'J': { 'id': 10, 'cls': 'rank-j' },
+            'Q': { 'id': 11, 'cls': 'rank-q' },
+            'K': { 'id': 12, 'cls': 'rank-k' },
+            'A': { 'id': 13, 'cls': 'rank-a' },
+        };
+        var _suits = {
+            'E': { 'id': 100, 'cls': '' },
+            'h': { 'id': 1, 'cls': 'suit-hearts' },
+            'c': { 'id': 2, 'cls': 'suit-clubs' },
+            'd': { 'id': 3, 'cls': 'suit-diams' },
+            's': { 'id': 4, 'cls': 'suit-spades' },
+        };
 
-                <div class="input-group-text">
-                    <div class="card playing rank-a suit-hearts">
-                        <div class="card-body">
-                            <div class="card-title"></div>
-                            <div class="card-text"></div>
-                        </div>
-                    </div>
-                    <div class="card playing rank-a suit-spades">
-                        <div class="card-body">
-                            <div class="card-title"></div>
-                            <div class="card-text"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="input-group-text">
-                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                        <label class="btn btn-secondary">
-                        <input type="checkbox" name="options" id="btnuseboard" autocomplete="off" checked> Доска: </label>
-                    </div>
-                </div>
+        return _parent.append($(
+            '<div/>', {
+                id: _id,
+                'class': _class + ' ' + _noms[_nom].cls + ' ' + _suits[_suit].cls,
+                append: $('<div/>', {
+                    'class': 'card-body',
+                }).append($('<div/>', {
+                    'class': 'card-title',
+                })).append($('<div/>', {
+                    'class': 'card-text',
+                })),
+            }));
+    };
+    var handcard1 = addplayingcard(handhtml, 'handcard1', 'A', 'h');
+    var handcard1 = addplayingcard(handhtml, 'handcard2', 'A', 's');
 
-                <div class="input-group-text">
-                    <div class="card playing rank-a suit-diams">
-                        <div class="card-body">
-                            <div class="card-title"></div>
-                            <div class="card-text"></div>
-                        </div>
-                    </div>
-                    <div class="card playing rank-a suit-clubs">
-                        <div class="card-body">
-                            <div class="card-title"></div>
-                            <div class="card-text"></div>
-                        </div>
-                    </div>
-                    <div class="card playing rank-2 suit-hearts">
-                        <div class="card-body">
-                            <div class="card-title"></div>
-                            <div class="card-text"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="input-group-text">
-                    <div class="card playing rank-k suit-clubs">
-                        <div class="card-body">
-                            <div class="card-title"></div>
-                            <div class="card-text"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="input-group-text">
-                    <div class="card playing emptyplace">
-                        <div class="card-body">
-                            <div class="card-title"></div>
-                            <div class="card-text"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    // board
+    $('<div>', {
+        'class': 'input-group-text',
+        append: $('<div/>', {
+            'class': 'btn-group btn-group-toggle',
+            append: $('<label/>', {
+                'class': 'btn btn-secondary',
+                text: ' Доска: ',
+                append: $('<input/>', {
+                    id: 'btnuseboard',
+                    type: 'checkbox',
+                    name: 'options'
+                }).attr({ 'autocomplte': 'off', 'checked': '' }),
+            }),
+        }).attr('data-toggle', 'buttons'),
+    }).appendTo('#tablecontainer');
+    var flophtml = $('<div/>', {
+        class: 'input-group-text',
+    }).appendTo('#tablecontainer');
+    var flopcard1 = addplayingcard(flophtml, 'flopcard1', 'A', 'd');
+    var flopcard2 = addplayingcard(flophtml, 'flopcard2', 'A', 'c');
+    var flopcard3 = addplayingcard(flophtml, 'flopcard3', '2', 'h');
 
-        </div>
-        */
+    var turnhtml = $('<div/>', {
+        class: 'input-group-text',
+    }).appendTo('#tablecontainer');
+    var turncard = addplayingcard(turnhtml, 'flopcard1', 'K', 'c');
+
+    var riverhtml = $('<div/>', {
+        class: 'input-group-text',
+    }).appendTo('#tablecontainer');
+    var rivercard = addplayingcard(riverhtml, 'flopcard1', 'E', 'E');
+
+    // deck
     $('<div/>', {
-        id: 'deckbtngroup',
-        'class': 'btn-group-vertical',
+        'class': 'input-group mb-3',
+        append: $('<div/>', {
+            'class': 'input-group-prepend',
+            append: $('<div/>', {
+                'class': 'input-group-text',
+                append: $('<div>', {
+                    id: 'deckbtngroup',
+                    'class': 'btn-group-vertical'
+                })
+            }),
+        }),
     }).appendTo('#deckwrapper');
     $.each(suits, function(i, suit) {
         $('<div/>', {
