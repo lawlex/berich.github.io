4a1892e6c315d83c5b080620e07491a6fee738b4
lawlex <alex.kuznecov@gmail.com>
2020-05-21 13:35:36 +0300

    Update

--- openbook/js/main.js
+++ openbook/js/main.js
@@ -14,7 +14,118 @@ $(document).ready(function() {
     var noms = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
     var nomsreverse = noms.reverse();
     var suits = ['h', 'c', 'd', 's'];
-    var suitcolors = ['danger', 'success', 'primary', 'dark']
+    var suitcolors = ['danger', 'success', 'primary', 'dark'];
+    $('<div/>', {
+        'class': 'input-group mb-3',
+        append: $('<div/>', {
+            id: 'tablecontainer',
+            'class': 'input-group-prepend',
+        }),
+    }).appendTo('#tablewrapper');
+    $('<div>', {
+        'class': 'input-group-text',
+        append: $('<div/>', {
+            'class': 'btn-group btn-group-toggle',
+            append: $('<label/>', {
+                'class': 'btn btn-secondary',
+                text: ' Рука: ',
+                append: $('<input/>', {
+                    id: 'btnusehand',
+                    type: 'checkbox',
+                    name: 'options'
+                }).attr({ 'autocomplte': 'off', 'checked': '' }),
+            }),
+        }).attr('data-toggle', 'buttons'),
+    }).appendTo('#tablecontainer');
+    var handhtml = $('<div/>', {
+        class: 'input-group-text',
+    }).appendTo('#tablecontainer');
+    var handcard1 = handhtml.append($(
+        `<div id='handcard1' class='card playing rank-a suit-hearts'>
+          <div class='card-body'>
+              <div class='card-title'></div>
+              <div class='card-text'></div>
+          </div>
+          </div>`, {}));
+    var handcard2 = handhtml.append($(
+        `<div id='handcard1' class='card playing rank-a suit-spades'>
+            <div class='card-body'>
+                <div class='card-title'></div>
+                <div class='card-text'></div>
+            </div>
+            </div>`, {}));
+
+    /*
+    <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <div class="input-group-text">
+                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-secondary">
+                        <input type="checkbox" name="options" id="btnusehand" autocomplete="off" checked> Рука: </label>
+                    </div>
+                </div>
+
+                <div class="input-group-text">
+                    <div class="card playing rank-a suit-hearts">
+                        <div class="card-body">
+                            <div class="card-title"></div>
+                            <div class="card-text"></div>
+                        </div>
+                    </div>
+                    <div class="card playing rank-a suit-spades">
+                        <div class="card-body">
+                            <div class="card-title"></div>
+                            <div class="card-text"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group-text">
+                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-secondary">
+                        <input type="checkbox" name="options" id="btnuseboard" autocomplete="off" checked> Доска: </label>
+                    </div>
+                </div>
+
+                <div class="input-group-text">
+                    <div class="card playing rank-a suit-diams">
+                        <div class="card-body">
+                            <div class="card-title"></div>
+                            <div class="card-text"></div>
+                        </div>
+                    </div>
+                    <div class="card playing rank-a suit-clubs">
+                        <div class="card-body">
+                            <div class="card-title"></div>
+                            <div class="card-text"></div>
+                        </div>
+                    </div>
+                    <div class="card playing rank-2 suit-hearts">
+                        <div class="card-body">
+                            <div class="card-title"></div>
+                            <div class="card-text"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group-text">
+                    <div class="card playing rank-k suit-clubs">
+                        <div class="card-body">
+                            <div class="card-title"></div>
+                            <div class="card-text"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group-text">
+                    <div class="card playing emptyplace">
+                        <div class="card-body">
+                            <div class="card-title"></div>
+                            <div class="card-text"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        */
     $('<div/>', {
         id: 'deckbtngroup',
         'class': 'btn-group-vertical',
